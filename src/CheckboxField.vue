12f80d630eeb756ef1c5cae83e7215c2a47a4711
Justin Kimbrell <jkimbrell@actengage.com>
2023-02-17 12:33:24 -0500

    Updates
    - Updated for Vue 3
    - Updated for monorepo
    - Updated for new FormControl
    - Updated Vite 4

--- src/CheckboxField.vue
+++ src/CheckboxField.vue
@@ -1,19 +1,100 @@
+<script lang="ts">
+import { FormControl } from '@vue-interface/form-control';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+    name: 'CheckboxField',
+
+    extends: FormControl,
+
+    props: {
+        /**
+         * The checked values
+         *
+         * @property String
+         */
+        checked: Boolean,
+
+        /**
+         * Add form-group wrapper to input.
+         */
+        group: {
+            type: Boolean,
+            default: false
+        },
+
+        /**
+         * The checked values.
+         *
+         * @property any
+         */
+        value: {
+            default: undefined
+        },
+
+        /**
+         * The class name assigned to the control element
+         *
+         * @property String
+         */
+        formControlClass: {
+            type: String,
+            default: 'form-check-input'
+        },
+
+        /**
+         * Display the form field and label inline
+         *
+         * @property Function
+         */
+        inline: Boolean,
+
+        /**
+         * The default label class assigned to the label element.
+         */
+        labelClass: {
+            type: [Object, String],
+            default: 'form-check-label'
+        },
+    },
+
+    // data: () => ({
+    //     checkedValues: []
+    // }),
+
+    computed: {
+        inlineClass() {
+            return this.inline && 'form-check-inline';
+        }
+    }
+});
+</script>
+
 <template>
-    <div :class="{[controlClass]: !!controlClass, [inlineClass]: inline}">
+    <div
+        class="form-check"
+        :class="{[inlineClass]: inline, ...formGroupClasses}">
         <input
-            :id="$attrs.id || hash"
+            :id="id"
             ref="field"
+            v-model="model"
             v-bind-events
             v-bind="controlAttributes"
-            type="checkbox"
             :value="value"
-            :checked="isChecked(value)"
-            @change="update">
+            type="checkbox">
 
-        <label :for="$attrs.id || hash" :class="{[computedLabelClass]: true, [labelClass]: true}">
-            <slot>{{ label }}</slot>
-        </label>
-
+        <slot name="label">
+            <label
+                ref="label"
+                :class="{ [labelClass]: true }"
+                :for="id">
+                <slot>
+                    {{ label }}
+                </slot>
+            </label>
+        </slot>
+            
         <slot name="feedback">
             <div 
                 v-if="invalidFeedback"
@@ -28,126 +109,11 @@
         </slot>
 
         <slot name="help">
-            <small v-if="helpText" ref="help">
+            <small
+                v-if="helpText"
+                ref="help">
                 {{ helpText }}
             </small>
         </slot>
     </div>
 </template>
-
-<script>
-import { FormControl } from '@vue-interface/form-control';
-
-export default {
-
-    name: 'CheckboxField',
-
-    mixins: [
-        FormControl
-    ],
-
-    model: {
-        prop: 'checkedValues',
-        event: 'change'
-    },
-
-    props: {
-        /**
-         * An array of event names that correlate with callback functions
-         *
-         * @property Function
-         */
-        bindEvents: {
-            type: Array,
-            default() {
-                return ['focus', 'blur', 'input', 'click', 'keyup', 'keydown', 'progress'];
-            }
-        },
-
-        /**
-         * The checked values
-         *
-         * @property String
-         */
-        checked: Boolean,
-
-        /**
-         * The checked values.
-         *
-         * @property String
-         */
-        checkedValues: {
-            type: Array,
-            default: () => []
-        },
-
-        /**
-         * The class name assigned to the control element
-         *
-         * @property String
-         */
-        defaultControlClass: {
-            type: String,
-            default: 'form-check'
-        },
-
-        /**
-         * Display the form field and label inline
-         *
-         * @property Function
-         */
-        inline: Boolean
-    },
-
-    // data: () => ({
-    //     checkedValues: []
-    // }),
-
-    computed: {
-
-        controlClasses() {
-            return {
-                [this.spacing]: !!this.spacing,
-                [this.inputClass]: !!this.inputClass,
-                ['is-valid']: !!(this.valid || this.validFeedback),
-                ['is-invalid']: !!(this.invalid || this.invalidFeedback),
-            };
-        },
-
-        computedLabelClass() {
-            return `${this.controlClass}-label`;
-        },
-
-        hash() {
-            return Math.random().toString(20).substr(2, 6);
-        },
-
-        inputClass() {
-            return `${this.controlClass}-input`;
-        },
-
-        inlineClass() {
-            return this.inline && `${this.controlClass}-inline`;
-        }
-    },
-
-    methods: {
-        isChecked(value) {
-            return Array.isArray(this.checkedValues) && this.checkedValues.indexOf(value) > -1;
-        },
-        update(event) {
-            if(!this.isChecked(event.target.value)) {
-                this.checkedValues.push(event.target.value);
-            }
-            else {
-                this.checkedValues.splice(
-                    this.checkedValues.indexOf(event.target.value), 1
-                );
-            }
-
-            this.$emit('change', this.checkedValues);
-            this.$emit('input', event);
-        }
-    }
-};
-</script>
